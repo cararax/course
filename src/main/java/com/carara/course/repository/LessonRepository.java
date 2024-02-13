ddf07940c635abfa170988adb6031b06c80bcb13
Pedro Carara <cararax@gmail.com>
2024-02-13 09:51:52 -0300

    feat: add custom query native methods

--- src/main/java/com/carara/course/repository/LessonRepository.java
+++ src/main/java/com/carara/course/repository/LessonRepository.java
@@ -2,8 +2,15 @@ package com.carara.course.repository;
 
 import com.carara.course.model.LessonModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface LessonRepository extends JpaRepository<LessonModel, UUID> {
+
+    @Query(value = "SELECT * FROM tb_lesson WHERE module_module_id = :moduleId", nativeQuery = true)
+    List<LessonModel> findAllLessonsIntoModule(@Param("moduleId") UUID moduleId);
+
 }
--- src/main/java/com/carara/course/repository/ModuleRepository.java
+++ src/main/java/com/carara/course/repository/ModuleRepository.java
@@ -2,8 +2,15 @@ package com.carara.course.repository;
 
 import com.carara.course.model.ModuleModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ModuleRepository extends JpaRepository<ModuleModel, UUID> {
+
+    @Query(value = "SELECT * FROM tb_module WHERE course_course_id = :courseId", nativeQuery = true)
+    List<ModuleModel> findAllModulesIntoCourse(@Param("courseId")UUID courseId);
+
 }
